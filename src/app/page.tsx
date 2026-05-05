d0b65d176230cb16204c2f401f97c4e1e293e6bb
ScanAlign <atifjan2019@gmail.com>
2026-05-05 23:10:40 +0500

    refactor: simplify product data handling in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,12 @@
-import Image from "next/image";
-import Link from "next/link";
 import { db } from "@/lib/db";
 import HomeSections from "./HomeSections";
+
+export const revalidate = 60;
 
 export default async function Home() {
   const [categories, productsResult] = await Promise.all([
     db.listCategories(),
     db.listProducts({ perPage: 8 }),
   ]);
-  const products = productsResult.data;
-
-  return <HomeSections categories={categories} products={products} />;
+  return <HomeSections categories={categories} products={productsResult.data} />;
 }
-
-// Static imports kept here so we can prefetch images via Next.
-export const dynamic = "force-static";
-export const revalidate = 60;
-void Image;
-void Link;
